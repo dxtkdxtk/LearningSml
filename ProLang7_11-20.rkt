9deac05a766c572fcaea7acf1dfe664d829f3338
jebin <cepinlin@gmail.com>
2014-12-09 20:56:38 +0800

    7-18 finish

--- ProLang7_11-20.rkt
+++ ProLang7_11-20.rkt
@@ -14,3 +14,35 @@
 
 (define mpr (mcons 1 (mcons #t "hi")))
 
+(define (my_delay th)
+  (mcons #f th))
+(define (my_force p)
+  (if (mcar p)
+      (mcdr p)
+      (begin (set-mcar! p #t)
+             (set-mcdr! p ((mcdr p )))
+             (mcdr p))))
+
+(define (bad_mult a b_thunk)
+  (cond [(= a 0) 0]
+        [(= a 1) (b_thunk)]
+        [#t (+ (b_thunk) (bad_mult (- a 1) b_thunk))]))
+             
+
+(define (slow_add x y)
+  (letrec ([slowadd (lambda (y z)
+                      (if (= z 0)
+                          y
+                          (slowadd y (- z 1))))])
+    (+ (slowadd x 50000000) y)))
+
+
+(define res2 (bad_mult 0 (let ([p (my_delay (lambda () (slow_add 4 5)))])
+                           (lambda () (my_force p)))))
+;(define res (bad_mult 999 (let ([p (my_delay (lambda () (slow_add 3 4)))])
+;                           (lambda () (my_force p)))))
+
+(define ones (lambda () (cons 1 ones)))
+(define (f1 x)  (cons x  (lambda () (f1 (+ x 1)))))
+(define nats (lambda () (f 1)))
+
