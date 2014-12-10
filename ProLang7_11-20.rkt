800415d721f08eb99817de008961424a635b7c9a
jebin <cepinlin@gmail.com>
2014-12-10 21:21:06 +0800

    7.18-7.20 finish

--- ProLang7_11-20.rkt
+++ ProLang7_11-20.rkt
@@ -46,3 +46,18 @@
 (define (f1 x)  (cons x  (lambda () (f1 (+ x 1)))))
 (define nats (lambda () (f 1)))
 
+
+
+(define fib (letrec ([memo null]
+                     [f (lambda (x)
+                          (let ([ans (assoc x memo)])
+                            (if ans
+                                (cdr ans)
+                                (let ([new-ans (if (or (= x 1) (= x 2))
+                                                   1
+                                                   (+ (f (- x 1)) (f (- x 2))))])
+                                  (begin
+                                    (set! memo (cons (cons x new-ans) memo))
+                                    new-ans)))))])
+              f))
+                          
